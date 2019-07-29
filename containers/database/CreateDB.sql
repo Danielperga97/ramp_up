70439db933f65a14af80b493aef1c0f6714c75b9
daniel <daniel@localhost.localdomain>
2019-07-29 09:00:30 -0500

    update dockerfiles

---
+++ containers/database/CreateDB.sql
@@ -0,0 +1,9 @@
+CREATE TABLE movies (
+    title character varying(255) NOT NULL,
+    release character varying (24) NOT NULL,
+    score integer varying(3),
+    reviewer character varying(70) NOT NULL,
+    publication character varying(70) NOT NULL
+);
+
+INSERT INTO movies (title,release,score,reviewer,publication) VALUES ("Suicide Squad","2016",8,"Robert Smith","The Daily Reviewer");
